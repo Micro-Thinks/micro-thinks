5d2e3a15be32c0429981ab216e67b2e92a39e846
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-10-17 16:49:33 +0500

    fixed

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -25,110 +25,91 @@ const ContacForm = () => {
       <div className="w-full h-[30vh] bg-blue-950 my-10 flex items-center">
         <h1 className="text-white pl-6 text-2xl font-semibold">Contact</h1>
       </div>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 w-[90vw]">
-          {/* Left Side: Map */}
-          <div className="h-[80vh] w-[40vw]">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=..."
-              width="100%"
-              height="100%"
-              frameBorder="0"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-            ></iframe>
+      <div className="flex justify-center items-center">
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name*"
+              value={formData.firstName}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+              required
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name*"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+              required
+            />
           </div>
-
-          {/* Right Side: Form */}
-          <div className="bg-white h-[70vh] w-[40vw]">
-            <form className="space-y-4" onSubmit={handleSubmit}>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  name="firstName"
-                  placeholder="First Name*"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                  required
-                />
-                <input
-                  type="text"
-                  name="lastName"
-                  placeholder="Last Name*"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                  required
-                />
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  name="company"
-                  placeholder="Company/Organisation*"
-                  value={formData.company}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                  required
-                />
-                <input
-                  type="url"
-                  name="website"
-                  placeholder="Website"
-                  value={formData.website}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                />
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email Address*"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                  required
-                />
-                <input
-                  type="tel"
-                  name="phone"
-                  placeholder="Phone*"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  className="border border-gray-300 p-3 rounded w-full"
-                  required
-                />
-              </div>
-              <input
-                type="text"
-                name="services"
-                placeholder="Services*"
-                value={formData.services}
-                onChange={handleChange}
-                className="border border-gray-300 p-3 rounded w-full"
-                required
-              />
-              <textarea
-                name="message"
-                placeholder="Tell us about your business."
-                value={formData.message}
-                onChange={handleChange}
-                className="border border-gray-300 p-3 rounded w-full"
-                rows="4"
-              ></textarea>
-              <button
-                type="submit"
-                className="bg-[#f79521] text-white px-4 py-2 rounded-full mt-4 w-1/6 translate-x-56"
-              >
-                Submit
-              </button>
-            </form>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="company"
+              placeholder="Company/Organisation*"
+              value={formData.company}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+              required
+            />
+            <input
+              type="url"
+              name="website"
+              placeholder="Website"
+              value={formData.website}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+            />
           </div>
-        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address*"
+              value={formData.email}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+              required
+            />
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Phone*"
+              value={formData.phone}
+              onChange={handleChange}
+              className="border border-gray-300 p-3 rounded w-full"
+              required
+            />
+          </div>
+          <input
+            type="text"
+            name="services"
+            placeholder="Services*"
+            value={formData.services}
+            onChange={handleChange}
+            className="border border-gray-300 p-3 rounded w-full"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Tell us about your business."
+            value={formData.message}
+            onChange={handleChange}
+            className="border border-gray-300 p-3 rounded w-full"
+            rows="4"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-[#f79521] text-white px-4 py-2 rounded-full mt-4 w-1/6 translate-x-56"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
